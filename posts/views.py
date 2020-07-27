db07eef5696ad7027576ebf9f33fbca7ca756a70
NDK1410 <khanh.nd1410@gmail.com>
2020-07-27 14:02:46 +0700

    showPost

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def index(request):
-    latest_post_list = Post.objects.order_by('-pub_date')[:4]
-    context = {'latest_post_list': latest_post_list}
-    return render(request, 'posts/index.html', context)
+	latest_post_list = Post.objects.order_by('-pub_date')[:4]
+	context = {'latest_post_list': latest_post_list}
+	return render(request, 'posts/index.html', context)
 
 def show(request, post_id):
-    return HttpResponse("You're looking at post %s." % post_id)
+	post = get_object_or_404(Post, pk=post_id)
+	return render(request, 'posts/show.html', {'post': post})

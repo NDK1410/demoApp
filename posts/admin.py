9c07b616a2052601e6cb77e87d48900e56861946
NDK1410 <khanh.nd1410@gmail.com>
2020-07-27 18:34:29 +0700

    customAdminPage

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+	fieldsets = [
+        ('Post Title', {'fields': ['title_text']}),
+        ('Post Content', {'fields': ['content_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Post, PostAdmin)

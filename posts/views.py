92f90a0e7ec6132968e7e0ea74c9a566015c8d99
NDK1410 <khanh.nd1410@gmail.com>
2020-07-27 11:25:31 +0700

    viewPost

--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
+
+def show(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)

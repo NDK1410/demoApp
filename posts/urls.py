b6e7d1462360d68d2a36fd23b5199f2b8c4ed949
NDK1410 <khanh.nd1410@gmail.com>
2020-07-27 14:43:40 +0700

    geneticViewsToLessCode

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     # path('<int:post_id>/', views.show, name='show'),
-    path('<int:post_id>/', views.show, name='show'),
+    # path('<int:post_id>/', views.show, name='show'),
+    path('', views.Index.as_view(), name='index'),
+    path('<int:pk>/', views.Show.as_view(), name='show'),
 ]

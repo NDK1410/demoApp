d95a9f62b10024ee4c674c5ff93fddccc5c58240
NDK1410 <khanh.nd1410@gmail.com>
2020-07-29 11:44:39 +0700

    pagination

--- posts/views.py
+++ posts/views.py
@@ -2,15 +2,19 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 class Index(generic.ListView):
     template_name = 'posts/index.html'
     context_object_name = 'latest_post_list'
+    paginator = Paginator(context_object_name, 4)
+    page_obj = paginator.get_page(page_number)
 
-    def get_queryset(self):
-        return Post.objects.order_by('-pub_date')[:6]
+    def get_queryset(self, request):
+        page_number = request.GET.get('page')
+        return (Post.objects.order_by('-pub_date')[:6], {'page_number':page_number})
 
 
 class Show(generic.DetailView):

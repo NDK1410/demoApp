c742a1901390bf9574ff5d6358da02fe1a3ed839
NDK1410 <khanh.nd1410@gmail.com>
2020-08-02 16:46:19 +0700

    register-login-logout

--- users/views.py
+++ users/views.py
@@ -17,4 +17,4 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(reverse("welcome"))
+            return redirect("/posts")
